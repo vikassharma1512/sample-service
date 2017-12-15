4f6e0eddff247305ee2f98d324f0e62e5ab8832d
Tom Howard <tom@windyroad.com.au>
2017-12-15 11:05:10 +1100

    clean up

--- src/test/java/io/swagger/samples/inflector/springboot/SpringProfileCucumber.java
+++ src/test/java/io/swagger/samples/inflector/springboot/SpringProfileCucumber.java
@@ -10,21 +10,21 @@ import org.springframework.test.context.ActiveProfiles;
 import cucumber.api.junit.Cucumber;
 
 public class SpringProfileCucumber extends Cucumber {
-	private Logger logger = LoggerFactory.getLogger(SpringProfileCucumber.class);
+  private Logger logger = LoggerFactory.getLogger(SpringProfileCucumber.class);
 
-	public SpringProfileCucumber(Class clazz) throws InitializationError, IOException {
-		super(clazz);
-		ActiveProfiles ap = (ActiveProfiles) clazz.getAnnotation(ActiveProfiles.class);
-		if (ap != null) {
-			String springProfilesActive = System.getProperty("spring.profiles.active");
-			logger.info("initial spring.profiles.active: {}", springProfilesActive);
-			String newActiveProfiles = String.join(",", ap.value());
-			String activeProfiles = newActiveProfiles;
-			if (springProfilesActive != null) {
-				activeProfiles = String.join(",", springProfilesActive, newActiveProfiles);
-			}
-			System.setProperty("spring.profiles.active", activeProfiles);
-			logger.info("current spring.profiles.active: {}", activeProfiles);
-		}
-	}
+  public SpringProfileCucumber(Class clazz) throws InitializationError, IOException {
+    super(clazz);
+    ActiveProfiles ap = (ActiveProfiles) clazz.getAnnotation(ActiveProfiles.class);
+    if (ap != null) {
+      String springProfilesActive = System.getProperty("spring.profiles.active");
+      logger.info("initial spring.profiles.active: {}", springProfilesActive);
+      String newActiveProfiles = String.join(",", ap.value());
+      String activeProfiles = newActiveProfiles;
+      if (springProfilesActive != null) {
+        activeProfiles = String.join(",", springProfilesActive, newActiveProfiles);
+      }
+      System.setProperty("spring.profiles.active", activeProfiles);
+      logger.info("current spring.profiles.active: {}", activeProfiles);
+    }
+  }
 }
